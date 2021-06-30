74d4ac01cf5f9ef5f35c787c9c04d217e95a07b6
aflame143 <djk8447@rit.edu>
2021-06-30 18:30:38 -0400

    Beginning of location picker

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+//API CALL FOR GOOGLE MAPS
+/*async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBPALFxbqB_W-JIppkis_peB-hxb6VLjxw&callback=initMap"
+  type="text/javascript"*/
+
+  //Script for search engine functionality using Google Search
+  //Copy the following code, and paste it into a <div> element in your site's <body> section, where you want both of the search box and the search results to render.
+  /*<script async src="https://cse.google.com/cse.js?cx=529107ffc92b02292"></script>
+<div class="gcse-search"></div>*/
